1cb37861dee020273586dc728e4923ded252186d
М.С <partansky@mail.ru>
2020-06-23 00:18:01 +0300

    conf.php

---
+++ conf.php
@@ -0,0 +1,6 @@
+<?php
+	define('PAGE', 0x100); // размер сегмента кода и он же размер сегмента данных
+	define('EXE_CS_OFFSET', 0x400); // начало сегмента кода в файле
+	define('EXE_DS_OFFSET', 0x600); // начало сегмента данных в файле
+	define('TASK_EXE_FULL_PATH', './task.exe');
+	define('JOBE_EXE_URL', 'http://46.188.11.165/');
